9a8f6fc53ebbf0a8130bc530a3e22eb07d9c1510
Olivier Mignault <olivier@mignault.ca>
2025-05-29 12:15:07 -0400

    Update orb.rb

--- orb.rb
+++ orb.rb
@@ -3,7 +3,7 @@ class Orb < Formula
 	homepage "https://orb.net"
 	url "https://pkgs.orb.net/earlyaccess/orb-osx-universal.zip"
 	sha256 "d54915cada76776bdb9574701f4566bd7404d82079bfcde31bf26b93ec669f63"
-	version "1.0.0" # or whatever version you have
+	version "1.0.1" # or whatever version you have
 
   def install
     bin.install "orb"
