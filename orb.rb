f26024ec6c3d0c2a4325800c5e143b45e9e52c5c
Olivier Mignault <olivier@mignault.ca>
2025-09-23 08:55:24 -0400

    Update Orb version to 1.3.1 and sha256 checksum

--- orb.rb
+++ orb.rb
@@ -1,6 +1,6 @@
 class Orb < Formula
-  version "1.3.0"
-  sha256 "24ee79cee3c44608600cc70c1f5ff13147e314fb96689d9cd4deb07666067c24"
+  version "1.3.1"
+  sha256 "0cba20d43d2d8afc8b362ce84b839460024e3023675835b2dea8b7a784136b52"
   
   desc "Network performance monitor"
   homepage "https://orb.net"
