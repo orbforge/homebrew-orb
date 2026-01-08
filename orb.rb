2afba3863cd787bcc515c456d698d3d85e6b7f44
Olivier Mignault <olivier@mignault.ca>
2026-01-08 13:07:36 -0500

    Bump Orb version to 1.4.0 with new SHA256

--- orb.rb
+++ orb.rb
@@ -1,6 +1,6 @@
 class Orb < Formula
-  version "1.3.9"
-  sha256 "3132decfa5ca2ef8e497a57491f59d3f627358fb2be0d5246171008eee7b5bca"
+  version "1.4.0"
+  sha256 "cd886e83576e32f7e8805237a3d63cc5f62f15a949a464c0e73c69ba4b86ec20"
   
   desc "Network performance monitor"
   homepage "https://orb.net"
