71e9f406342f733f4b76d2f1836a524cf48f5888
Wayne Bills <1715988+wbills@users.noreply.github.com>
2026-06-24 12:46:19 -0500

    V1.5.0 (#5)
    * bump to sensor 1.5.0
    
    * update sha
    
    ---------
    
    Co-authored-by: wbills <wbills@users.noreply.github.com>

--- orb.rb
+++ orb.rb
@@ -1,7 +1,7 @@
 class Orb < Formula
-  version "1.4.13"
-  sha256 "b668d54f72953c7787012428db147071bdd5483361685b68adc7f16d87ed172e"
-  
+  version "1.5.0"
+  sha256 "f4f3ebd2a04b48fcb81039e5bed5d7a214d2102704c301ea14889d53d68b833d"
+
   desc "Network performance monitor"
   homepage "https://orb.net"
   url "https://pkgs.orb.net/stable/macos/orb-#{version}.zip"
