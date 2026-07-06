416da0db9fb1f867b2dd774143b732ef6682312f
Wayne Bills <1715988+wbills@users.noreply.github.com>
2026-07-06 18:55:21 -0500

    bump to v1.5.2 sensor release (#6)
    Co-authored-by: wbills <wbills@users.noreply.github.com>

--- orb.rb
+++ orb.rb
@@ -1,6 +1,6 @@
 class Orb < Formula
-  version "1.5.0"
-  sha256 "f4f3ebd2a04b48fcb81039e5bed5d7a214d2102704c301ea14889d53d68b833d"
+  version "1.5.2"
+  sha256 "8377093a270122c540ccfbfee5e41c1607646d67c8acbedc068a971a33e76754"
 
   desc "Network performance monitor"
   homepage "https://orb.net"
