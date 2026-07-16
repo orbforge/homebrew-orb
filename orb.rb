7d0e2b047fc3aaeac2f529536a24dfe25d71761e
Wayne Bills <1715988+wbills@users.noreply.github.com>
2026-07-16 08:42:19 -0500

    bump to 1.5.3 (#7)
    Co-authored-by: wbills <wbills@users.noreply.github.com>

--- orb.rb
+++ orb.rb
@@ -1,6 +1,6 @@
 class Orb < Formula
-  version "1.5.2"
-  sha256 "8377093a270122c540ccfbfee5e41c1607646d67c8acbedc068a971a33e76754"
+  version "1.5.3"
+  sha256 "9431c4a0afaf7095b71022f9a60b7680c8c3524361649a87a79ee1bc57d58e93"
 
   desc "Network performance monitor"
   homepage "https://orb.net"
